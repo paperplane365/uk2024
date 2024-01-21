70723c6735fa1ac583f5649776414129d5f164cd
Vityaz-XII <setiawan2206>
2024-01-22 01:39:41 +0700

    ukk intiv

---
+++ dist/js/script.js
@@ -0,0 +1,63 @@
+// JANGAN ERROR TERUS LAH ASTAGFIRULLAAAAH//
+// jS NGAPA HARD DAHHH ?//
+// NGEN***😇//
+
+// Navbar Fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector('#to-top');
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+};
+
+// Hamburger
+const Hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+Hamburger.addEventListener("click", function () {
+  Hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+
+// Klik di Luar Menunya
+window.addEventListener('click', function(e) {
+if(e.target != Hamburger && e.target != navMenu) {
+  Hamburger.classList.remove('hamburger-active');
+  navMenu.classList.add('hidden');
+}
+});
+
+
+// DarkMode Toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function() {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+});
+
+
+// Pindahkan Posisi Toggle Sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark')
+  darkToggle.checked = true;
+} else {
+  
+  darkToggle.checked = falses;
+}
